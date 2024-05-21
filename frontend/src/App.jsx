0dafa833d40afa5e88e1e02a2d7d9d74c817eb6a
Yacheru <amilohin274@gmail.com>
2024-05-21 11:00:31 +0300

    stats template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Root from "./pages/root/Root.jsx";
 import Term from "./pages/terms/Terms.jsx";
 import Bans from "./pages/bans/Bans.jsx";
+import Stats from "./pages/stats/Stats.jsx";
 
 import './pages/pages.css'
 
@@ -13,6 +14,7 @@ export default function App() {
             <Route path='*' element={<Root />} />
             <Route path='/terms' element={<Term />} />
             <Route exact path='/bans' element={<Bans />} />
+            <Route path='/stats' element={<Stats />} />
         </Routes>
     );
 }
---
+++ frontend/src/pages/stats/Stats.jsx
@@ -0,0 +1,15 @@
+import './Stats.css'
+import Header from "../root/components/header/header.jsx";
+
+export default function Stats() {
+    return (
+        <>
+            <div className={'container'}>
+                <Header />
+                <div className={'flex center in-dev'}>
+                    IN DEV
+                </div>
+            </div>
+        </>
+    )
+}
