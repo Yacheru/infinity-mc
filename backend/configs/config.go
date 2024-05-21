775ac49f730b86429e6fa8c3cb542b50b960c972
Yacheru <amilohin274@gmail.com>
2024-05-21 14:00:05 +0300

    config.json Not Found fix

--- backend/configs/config.go
+++ backend/configs/config.go
@@ -5,6 +5,6 @@ import "github.com/spf13/viper"
 // InitConfig Инициализирует работу с конфигурационным файлом
 func InitConfig() error {
 	viper.AddConfigPath("configs")
-	viper.SetConfigName("config.json")
+	viper.SetConfigName("config")
 	return viper.ReadInConfig()
 }
